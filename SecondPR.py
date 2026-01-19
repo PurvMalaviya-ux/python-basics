4719347a3f1868337c8b4ae2ed1f7ba78bc13df3
PurvMalaviya <purvmalaviya69@gmail.com>
2026-01-19 22:02:13 +0530

    Add files via upload

---
+++ SecondPR.py
@@ -0,0 +1,16 @@
+print("You can Find addition and multipication ")
+Name =input("What is Your Good Name :")
+print("                             ")#for Space between Two lines 
+print("                             ")
+print("First Let's Find Addition", Name)
+print("                             ")
+No1 = int(input("Enter First Number :"))
+No2 = int(input("Enter Second Number :"))
+print("                             ")
+Sum = No1+No2
+Mlp = No1*No2
+print("                             ")
+print("Congratulation , We Calculated Them")
+print("                             ")
+print("The Addition of your Number is ", Sum)
+print("The Multipication of your Number is ", Mlp)
