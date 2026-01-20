3035b9d10993a08fa9e216e7237a5f380dd6d912
PurvMalaviya <purvmalaviya69@gmail.com>
2026-01-20 12:12:04 +0530

    Add files via upload

--- PasswordMaker.py
+++ PasswordMaker.py
@@ -11,17 +11,17 @@ print("                        ")
 
 
 length = len(Password)
-if length >= 8:
-    print(f"Hello {Name} Your Password Is Weak (Add Minimum 8 Charactors)")
+if length <= 8:
+    print(f"Hello {Name}, Your Password Is Weak (Add Minimum 8 Charactors)")
 elif not any(char.isdigit() for char in Password):
-    print(f"Hello {Name} Your Password is Weak (Add Digits)")
+    print(f"Hello {Name}, Your Password is Weak Purv(Add Digits)")
 elif not any(char.isupper() for char in Password):
-    print(f"Hello {Name} your Password Is weak (Add Uppercase Charactor)")
+    print(f"Hello {Name}, Your Password Is weak (Add Uppercase Charactor)")
 elif not any(char.islower() for char in Password):
-    print(f"Hello {Name} your Password is weak (add Lowercase charactor)")
+    print(f"Hello {Name}, Your Password is weak (add Lowercase charactor)")
 elif not any(char in " !@#$%^&*()+=`~:;'~`<,>.:;?/{[]} " for char in Password):
-    print(f"Hello {Name}Your Password is Weak (add Some Special Charactors)")
+    print(f"Hello {Name}, Your Password is Weak (add Some Special Charactors)")
 else:
-    print(f"Hello {Name}your Password is Strong")
+    print(f"Hello {Name} Your Password is Strong")
     print()
-    print(f"Thanks {Name} For Using My Code")
+    print(f"Thanks {Name}, For Using My Code")
